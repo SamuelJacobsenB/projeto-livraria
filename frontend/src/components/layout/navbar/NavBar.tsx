bafbd36fe3f025f8fe2764d5b55a490374f42fe1
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-08-05 12:34:52 -0300

    Navbar

---
+++ frontend/src/components/layout/navbar/NavBar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+//------------------------------------------------------
+import './NavBar.module.css';
+//------------------------------------------------------
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+        <div>
+
+        </div>
+        <div>
+            <div>
+                <Link href={''}></Link>
+                <Link href={''}></Link>
+                <Link href={''}></Link>
+            </div>
+        </div>
+    </nav>
+  );
+};
+//------------------------------------------------------
+export default NavBar;
